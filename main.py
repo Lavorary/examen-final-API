eaaae378a0f5d149b3bd8b7c7487746eb2a8c8bc
Lavorary <hei.lavorary@gmail.com>
2025-09-03 11:35:46 +0300

    feat: app.get("/ping")

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import base64
+import numbers
 from typing import List
 
 from fastapi import FastAPI
@@ -7,3 +8,9 @@ from starlette.requests import Request
 from starlette.responses import Response, JSONResponse
 
 app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return Response(content="pong", status_code=200, media_type="text/plain")
+
+
