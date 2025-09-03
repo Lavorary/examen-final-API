7bb445712503eb39d9209e1941d856587a02e7b8
Lavorary <hei.lavorary@gmail.com>
2025-09-03 11:55:53 +0300

    feat: app.post("/cars")

--- main.py
+++ main.py
@@ -10,19 +10,48 @@ from starlette.responses import Response, JSONResponse
 app = FastAPI()
 
 @app.get("/ping")
-async def ping():
+def ping():
     return Response(content="pong", status_code=200, media_type="text/plain")
 
 
+
+
 class Characteristic(BaseModel):
-    max_speed: numbers.Number
-    min_speed: numbers.Number
+    max_speed: int
+    min_fuel_capacity: int
 
 
-@app.post("/cars")
-async def cars():
+class Cars:
     identifier : str
     brand : str
     model : str
     characteristics : Characteristic
+
+
+cars_stored = List[Cars] = []
+
+
+def car_serialized():
+    car_serialized = []
+    for c in cars_stored:
+        car_serialized.append(c.model_dump())
+    return car_serialized
+
+
+
+@app.post("/cars")
+def cars(car_paylod: List[Cars]):
+    cars_stored.append(car_paylod)
+    serilized_car = car_serialized()
+    return JSONResponse(content=serilized_car, status_code=201, media_type="application/json")
+
+
+
+
+
+
+
+
+
+
 
